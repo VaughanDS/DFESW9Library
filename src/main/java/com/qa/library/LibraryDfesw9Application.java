10ba65e915f87c00429b3298ec3aa1cd53b645fb
VaughanDS <vaughandesousa@gmail.com>
2022-03-16 12:03:47 +0000

    Initial commit

---
+++ src/main/java/com/qa/library/LibraryDfesw9Application.java
@@ -0,0 +1,13 @@
+package com.qa.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDfesw9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryDfesw9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/library/LibraryDfesw9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDfesw9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
